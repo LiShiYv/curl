fb53d396be99f374bd1db8fe9fd1e745c1c365d6
LiShiYv <qq15354304250@163.com.com>
2019-03-23 10:48:47 +0800

    app

--- app/Http/Middleware/CheckCookies.php
+++ app/Http/Middleware/CheckCookies.php
@@ -11,9 +11,8 @@ class CheckCookies
     public function handle($request,Closure $next){
         if(isset($_COOKIE['id'])&& isset($_COOKIE['token'])){
             $key ='str:u:token:'.$_COOKIE['id'];
-            print_r($key);die;
-            $token =Redis::hget($key,'web');
-            //var_dump($token);die;
+            //print_r($key);die;
+            $token =Redis::hGet($key,'web');
             if($_COOKIE['token']==$token){
                 $request->attributes->add(['is_login'=>1]);
             }else{
